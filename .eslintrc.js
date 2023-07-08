edbe5a05c1ce2be4c01426a2ea4822edd85608b5
ppajor <pawel.pajor@makersden.io>
2023-07-08 23:16:01 +0200

    fix: config adjustments

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,11 +12,8 @@ module.exports = {
     ],
     extends: [
       'eslint:recommended',
-      'next',
-      'next/core-web-vitals',
       'plugin:@typescript-eslint/recommended',
       'prettier',
-      'plugin:storybook/recommended',
     ],
     rules: {
       'no-unused-vars': 'off',
--- App.tsx
+++ App.tsx
@@ -1,20 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text className='text-red-700'>Initial app!</Text>
+    <View className='flex items-center justify-center h-full w-full'>
+      <Text className='text-red-700 flex'>Initial app! </Text>
       <StatusBar style='auto' />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
